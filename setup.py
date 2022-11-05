4d5410e0d0d8e8f56a6a618b5db3c1bd10071cb8
Shengyu Huang <felixhuang101@gmail.com>
2022-11-05 19:43:39 -0400

    third commit

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="felixlibrary",
-    version="0.0.2",
+    version="0.0.7",
     description="Demo library",
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     long_description_content_type="text/markdown",
