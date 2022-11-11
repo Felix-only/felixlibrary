8e5ac1f1460a436c14472d5fd4b935d196c7eddd
Shengyu Huang <felixhuang101@gmail.com>
2022-11-10 20:06:37 -0500

    changes after successfully running the code

--- felix_library/main.py
+++ felix_library/main.py
@@ -154,49 +154,49 @@ def main(currency_pairs):
     initialize_aggregated_tables(engine,currency_pairs)
     
     # Open a RESTClient for making the api calls
-    with RESTClient(key) as client:
-        # Loop that runs until the total duration of the program hits 24 hours. 
-        while count < 86400: # 86400 seconds = 24 hours
-            
-            # Make a check to see if 6 minutes has been reached or not
-            if agg_count == 360:
-                # Aggregate the data and clear the raw data tables
-                aggregate_raw_data_tables(engine,currency_pairs)
-                reset_raw_data_tables(engine,currency_pairs)
-                agg_count = 0
-            
-            # Only call the api every 1 second, so wait here for 0.75 seconds, because the 
-            # code takes about .15 seconds to run
-            time.sleep(0.75)
-            
-            # Increment the counters
-            count += 1
-            agg_count +=1
+    client = RESTClient(key)
+    # Loop that runs until the total duration of the program hits 24 hours. 
+    while count < 86400: # 86400 seconds = 24 hours
 
-            # Loop through each currency pair
-            for currency in currency_pairs:
-                # Set the input variables to the API
-                from_ = currency[0]
-                to = currency[1]
+        # Make a check to see if 6 minutes has been reached or not
+        if agg_count == 360:
+            # Aggregate the data and clear the raw data tables
+            aggregate_raw_data_tables(engine,currency_pairs)
+            reset_raw_data_tables(engine,currency_pairs)
+            agg_count = 0
 
-                # Call the API with the required parameters
-                try:
-                    resp = client.forex_currencies_real_time_currency_conversion(from_, to, amount=100, precision=2)
-                except:
-                    continue
+        # Only call the api every 1 second, so wait here for 0.75 seconds, because the 
+        # code takes about .15 seconds to run
+        time.sleep(0.75)
 
-                # This gets the Last Trade object defined in the API Resource
-                last_trade = resp.last
+        # Increment the counters
+        count += 1
+        agg_count +=1
 
-                # Format the timestamp from the result
-                dt = ts_to_datetime(last_trade["timestamp"])
+        # Loop through each currency pair
+        for currency in currency_pairs:
+            # Set the input variables to the API
+            from_ = currency[0]
+            to = currency[1]
 
-                # Get the current time and format it
-                insert_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-                
-                # Calculate the price by taking the average of the bid and ask prices
-                avg_price = (last_trade['bid'] + last_trade['ask'])/2
+            # Call the API with the required parameters
+            try:
+                resp = client.get_real_time_currency_conversion(from_, to, amount=100, precision=2)
+            except:
+                continue
 
-                # Write the data to the SQLite database, raw data tables
-                with engine.begin() as conn:
-                    conn.execute(text("INSERT INTO "+from_+to+"_raw(ticktime, fxrate, inserttime) VALUES (:ticktime, :fxrate, :inserttime)"),[{"ticktime": dt, "fxrate": avg_price, "inserttime": insert_time}])
+            # This gets the Last Trade object defined in the API Resource
+            last_trade = resp.last
+
+            # Format the timestamp from the result
+            dt = ts_to_datetime(last_trade.timestamp)
+
+            # Get the current time and format it
+            insert_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+            # Calculate the price by taking the average of the bid and ask prices
+            avg_price = (last_trade.bid + last_trade.ask)/2
+
+            # Write the data to the SQLite database, raw data tables
+            with engine.begin() as conn:
+                conn.execute(text("INSERT INTO "+from_+to+"_raw(ticktime, fxrate, inserttime) VALUES (:ticktime, :fxrate, :inserttime)"),[{"ticktime": dt, "fxrate": avg_price, "inserttime": insert_time}])
